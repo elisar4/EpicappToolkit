57d7649bf127639c19968d125f22bd0d5284dc7d
Vladimir <bobah1991@gmail.com>
2018-08-11 05:24:31 +0300

    Initial Commit

---
+++ EpicappToolkit/EpicappToolkit.h
@@ -0,0 +1,19 @@
+//
+//  EpicappToolkit.h
+//  EpicappToolkit
+//
+//  Created by Vladimir Roganov on 11/08/2018.
+//  Copyright © 2018 Vladimir Roganov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EpicappToolkit.
+FOUNDATION_EXPORT double EpicappToolkitVersionNumber;
+
+//! Project version string for EpicappToolkit.
+FOUNDATION_EXPORT const unsigned char EpicappToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EpicappToolkit/PublicHeader.h>
+
+
