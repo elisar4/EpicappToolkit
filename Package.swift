8283689bf9a9ca02bea543fc92437bee95a9f02b
Vladimir <bobah1991@gmail.com>
2019-12-14 05:55:10 +0300

    update supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "EpicappToolkit",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         .library(
             name: "EpicappToolkit",
